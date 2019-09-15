978d69adb736b776af3a0132dea6d158fd75b72c
HeyHeyIryna <neklesa_iryna@ukr.net>
2019-09-15 17:40:56 +0000

    js

---
+++ js/index.js
@@ -0,0 +1,10 @@
+document.getElementById('nav-about').addEventListener('click', () => location='#about');
+document.getElementById('nav-our-cakes').addEventListener('click', () => location='#our-cakes');
+document.getElementById('nav-reviews').addEventListener('click', () => location='#reviews');
+document.getElementById('nav-contacts').addEventListener('click', () => location='#contacts');
+
+
+
+
+
+
